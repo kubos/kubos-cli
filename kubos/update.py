074841c01eae6bddbf1b9aa1e7c6a9ad847b9881
kyleparrott <kyle@kubos.co>
2016-10-24 10:44:22 -0500

    KUBOS-173 Relink targets and modules after pulling the latest tags and checking out the latest source version.

--- kubos/update.py
+++ kubos/update.py
@@ -13,8 +13,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import argparse
 import git
 import json
+import logging
 import sys
 import time
 import os
@@ -23,12 +25,18 @@ from kubos.utils.git_common import *
 from kubos import version as kubos_version
 from options import parser
 from packaging import version
+from yotta import link, link_target
+from yotta.lib import detect
 
 def addOptions(parser):
     pass
 
 
 def execCommand(args, following_args):
+    if os.geteuid() != 0:
+        #After changing the version the new target/modules are re-linked to /usr/local/lib.. which requires root to succeed
+        print 'Woops, this command needs to be run as root. Please re-run `sudo kubos update`'
+        sys.exit(1)
     if not os.path.isdir(KUBOS_DIR):
         os.makedirs(KUBOS_DIR)
     os.chdir(KUBOS_DIR)
@@ -49,3 +57,44 @@ def execCommand(args, following_args):
     except git.exc.GitCommandError as e:
         print 'Error: there was an error accessing the remote git repository...'
         print 'The specific error is: \n\n %s' % e
+    relink_modules(KUBOS_SRC_DIR)
+
+
+def relink_modules(path):
+    logging.disable(logging.WARNING)
+    for subdir in os.listdir(path):
+        cur_dir = os.path.join(path, subdir)
+        if os.path.isdir(cur_dir):
+            relink_modules(cur_dir)
+        elif subdir == 'module.json':
+            link_module_globally(cur_dir)
+        elif subdir == 'target.json':
+            link_target_globally(cur_dir)
+
+
+def link_target_globally(path):
+    path = os.path.dirname(path)
+    start_dir = os.getcwd()
+    os.chdir(path)
+    link_target_args = argparse.Namespace(target_or_path=None,
+                                          config=None,
+                                          target=detect.kubosDefaultTarget(),
+                                          save_global=True,
+                                          no_install=False)
+    link_target.execCommand(link_target_args, '')
+    os.chdir(start_dir)
+
+
+def link_module_globally(path):
+    start_dir = os.getcwd()
+    path_dir_name = os.path.dirname(path)
+    os.chdir(path_dir_name)
+    with open(path, 'r') as json_file:
+        data = json.load(json_file)
+        module_name = data['name']
+    link_args = argparse.Namespace(module_or_path=path_dir_name,
+                                   config=None,
+                                   target=detect.kubosDefaultTarget())
+    link.execCommand(link_args, None)
+    os.chdir(start_dir)
+
