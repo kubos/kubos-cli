5de6d445aad12ef6819d5281d6a0f6d5f712fc1b
kyleparrott <kyle@kubos.co>
2017-01-11 09:33:22 -0600

    Adding default arguments to the update unittest

--- test/test_update.py
+++ test/test_update.py
@@ -1,11 +1,27 @@
-import os, sys
+import os, sys, imp
 
 myPath = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, myPath + '/../kubos/utils/')
+sys.path.insert(0, myPath + '/../')
+sys.path.append(myPath + '/../kubos/utils/')
 
-from git_common import *
+import argparse
+import kubos
 from kubos import update
+from git_common import *
+import shutil
 
-def test_creates_kubos_dir():
-    result = update.execCommand([], [])
-    assert os.path.isdir(KUBOS_DIR)
+
+class TestUpdate(object):
+    def setup_class(cls):
+      if os.path.isdir(KUBOS_DIR):
+        shutil.rmtree(KUBOS_DIR)
+
+
+    def test_creates_kubos_dir(self):
+        assert not (os.path.isdir(KUBOS_DIR))
+        #Yotta will pass in default argument values so we have to mock the default set_version argument value
+        args = argparse.Namespace()
+        args.set_version = None
+        kubos.update.execCommand(args, None)
+        assert os.path.isdir(KUBOS_DIR)
+
